a55023599809a4a25d1a7b8b7b900c2bef8e5265
PowerMtest <powermtest@gmail.com>
2018-02-17 20:58:52 +0100

    trzzeba kod zrefaktoryzowac - utworzona metoda, ale jest pelno bledow

--- Lecture_63_Strings/Program.cs
+++ Lecture_63_Strings/Program.cs
@@ -10,7 +10,47 @@ namespace Lecture_63_Strings
     {
         static void Main(string[] args)
         {
-            var full
+            var fullName = "Ajem Debest ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim()); //usuwa spacje na koncu stringa
+            Console.WriteLine("Trim: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("Imie: " +firstName );
+            Console.WriteLine("Nazwisko: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("(prostsza metoda) Imie: " +names[0]);
+            Console.WriteLine("(prostsza metoda) Nazwisko: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Ajem", "I am"));
+
+            if (string.IsNullOrWhiteSpace("")) //jak dziala obrona przed spacjami
+            {
+                Console.WriteLine("Nierawidlowe");
+            }
+
+            //konwersja stringow
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("zaczynamy konwersje stringow");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 25.78f;
+            
+            Console.WriteLine(price.ToString("C"));
+            Console.WriteLine(price.ToString("C0"));
+
+
+
+
+
+
+
         }
     }
 }
---
+++ Lecture_64_Summarizing_text/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_64_Summarizing_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                "to bedzie naprawde zajebiscie zajebiscie ale to naprawde zajebiscie dlugi String. Co prawda to nie jest prawda, ale na potrzeby tego cwiczenia uznajmy, ze jest tak jak tutaj jest napisane";
+            
+            }
+        }
+
+        static string SummarizeText(string text)
+        {
+        const int maxLength = 20;
+            if (sentence.Length < maxLength)
+            {
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
+
+        }
+    }
+
+}
