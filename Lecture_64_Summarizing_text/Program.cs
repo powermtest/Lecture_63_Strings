a24f9b5c890ed4f6fb7385f0150d54106c996189
PowerMtest <powermtest@gmail.com>
2018-02-17 21:35:57 +0100

    Wszystko ladnie z nowa klasa i bez bledow. Koniec wykladu 64

--- Lecture_64_Summarizing_text/Program.cs
+++ Lecture_64_Summarizing_text/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lecture_64_Summarizing_text
 {
@@ -12,38 +9,11 @@ namespace Lecture_64_Summarizing_text
         {
             var sentence =
                 "to bedzie naprawde zajebiscie zajebiscie ale to naprawde zajebiscie dlugi String. Co prawda to nie jest prawda, ale na potrzeby tego cwiczenia uznajmy, ze jest tak jak tutaj jest napisane";
-            
-            }
+            var summary = StringUtility.SummarizeText(sentence, 31);
+            Console.WriteLine(summary);
         }
 
-        static string SummarizeText(string text)
-        {
-        const int maxLength = 20;
-            if (sentence.Length < maxLength)
-            {
-                Console.WriteLine(sentence);
-            }
-            else
-            {
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
-
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > maxLength)
-                    {
-                        break;
-                    }
-                }
-
-                var summary = String.Join(" ", summaryWords) + "...";
-                Console.WriteLine(summary);
-
-        }
+        
     }
 
 }
